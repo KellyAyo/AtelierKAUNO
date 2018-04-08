0f261b447a65592513a8c30d7a7c96ddab822e82
Phillip Kelly-Ayo <Kelly-Ayo@MacBook-Pro.local>
2018-04-08 18:10:39 +0100

    First 5 files added to framework

---
+++ AtelierKAUNO/AtelierKAUNO/AtelierKAUNO.h
@@ -0,0 +1,19 @@
+//
+//  AtelierKAUNO.h
+//  AtelierKAUNO
+//
+//  Created by Phillip Kelly-Ayo on 08/04/2018.
+//  Copyright © 2018 Phillip Kelly-Ayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AtelierKAUNO.
+FOUNDATION_EXPORT double AtelierKAUNOVersionNumber;
+
+//! Project version string for AtelierKAUNO.
+FOUNDATION_EXPORT const unsigned char AtelierKAUNOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AtelierKAUNO/PublicHeader.h>
+
+
